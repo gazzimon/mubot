54a2b3f028d93a40f075144eeb19dbb395b595ac
unknown <gazzimon@gmail.com>
2026-06-03 10:38:49 -0300

    payload mod prod

--- server.js
+++ server.js
@@ -872,8 +872,8 @@ function buildAdminLightingPayload(input = {}) {
     'Reclamo generado desde endpoint admin de prueba.'
   ].filter(Boolean).join(' ');
 
-  const areaServicioId = MUNIDIGITAL_ENV === 'PROD' ? 6878 : 7916;
-  const origenId = MUNIDIGITAL_ENV === 'PROD' ? null : 149;
+  const areaServicioId = MUNIDIGITAL_ENV === 'PROD' ? 7868 : 7916;
+  const origenId = MUNIDIGITAL_ENV === 'PROD' ? 156 : 149;
 
   return {
     direccion,
